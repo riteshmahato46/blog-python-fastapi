35d48c3c9519d17c073b0de9f7acb2ba91aa95bf
riteshmahato2012@gmail.com <riteshmahato2012@gmail.com>
2023-01-12 22:32:16 +0530

    Return user to the controller after authentication

--- app/oauth2.py
+++ app/oauth2.py
@@ -2,13 +2,14 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from . import schemas
+from sqlalchemy.orm import Session
+from . import schemas, database, models
 
 # to get a string like this run:
 # openssl rand -hex 32
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
@@ -36,8 +37,12 @@ def verify_access_token(token: str, credentials_exception):
     
     return token_data
     
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                             detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
     
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+    
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    
+    return user
--- app/routers/post_router.py
+++ app/routers/post_router.py
@@ -10,7 +10,7 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.PostResponse])
-def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     #cursor.execute("""SELECT * FROM posts """)
     #posts =cursor.fetchall()
 
@@ -18,12 +18,12 @@ def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_c
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
-def create_posts(post:schemas.Post, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def create_posts(post:schemas.Post, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     #cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
      #              (post.title, post.content, post.published))
     #new_post = cursor.fetchone()
     #conn.commit()
-    print(user_id)
+    print(current_user.email)
     new_post = models.Post2(**post.dict())
     db.add(new_post)
     db.commit()
@@ -31,7 +31,7 @@ def create_posts(post:schemas.Post, db: Session = Depends(get_db), user_id: int 
     return new_post
 
 @router.get("/{id}", response_model=schemas.PostResponse)
-def get_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
  #   cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
 #    post = cursor.fetchone()
     
@@ -43,7 +43,7 @@ def get_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oaut
     return post
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
  #   cursor.execute("""DELETE FROM posts WHERE id = %s returning *""",(str(id),))
  #   deleted_post = cursor.fetchone()
  #   conn.commit()
@@ -58,7 +58,7 @@ def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(o
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}", response_model=schemas.PostResponse)
-def update_post(id: int, post: schemas.Post, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def update_post(id: int, post: schemas.Post, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     #cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
     #               (post.title, post.content, post.published, str(id)))
     #updated_post = cursor.fetchone()
