89e289c318f4e2d768b667e9f3182b39bfee9b24
riteshmahato2012@gmail.com <riteshmahato2012@gmail.com>
2023-01-13 14:28:24 +0530

    Add title searching via query param

--- app/routers/post_router.py
+++ app/routers/post_router.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from fastapi import Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from .. import oauth2, schemas
@@ -12,7 +12,7 @@ router = APIRouter(
 
 @router.get("/", response_model=List[schemas.PostResponse])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), 
-              limit: int = 10, skip: int = 0):
+              limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     '''
     Summary:
     Gets all the posts from the database.
@@ -24,7 +24,7 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
     List[PostResponse] : A list of PostResponse types
     '''
     print(limit)
-    posts = db.query(models.Post).limit(limit).offset(skip).all()
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 
