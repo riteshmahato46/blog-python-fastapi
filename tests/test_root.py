4b60a1f2445e8028fc77311291f4207089400ca0
riteshmahato2012@gmail.com <riteshmahato2012@gmail.com>
2023-01-19 18:04:24 +0530

    fix sample test in build

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,5 +1,8 @@
+
 from app.main import ping
+
+
 def test_ping():
-    result = ping()
-    print(result)
-    assert result == {"Ping": "Ok"}
+    str = ping()
+    result = {"Ping": "Ok"}
+    assert str == result
